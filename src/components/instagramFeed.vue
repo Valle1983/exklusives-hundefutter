b7483a2d366b7b0c9036ac280bb91162386be12b
Valerie Unger <Valerie.unger@gmx.net>
2025-05-13 10:50:06 +0200

    updae key instagram

--- src/components/instagramFeed.vue
+++ src/components/instagramFeed.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="instagram-section">
-    <h2 class="text-center text-h5 text-weight-bold q-mb-lg">
+    <h2 class="text-center text-h5 text-white text-weight-bold q-mb-lg">
       Unsere Instagram-Seite
     </h2>
 
